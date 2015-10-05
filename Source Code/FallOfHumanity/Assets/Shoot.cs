20c6b3253af0bd958e36fc7127fe79798e86dc15
misterash <THEMisterAsh@live.co.uk>
2015-10-05 16:38:47 +0100

    Make Cube Shoot! Done. Create Deleter Object! Done.

---
+++ Source Code/FallOfHumanity/Assets/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+	
+	public GameObject prefab;
+	float distance = 10.0f;
+	float bulletLife = 6.0f;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+			
+			Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+			position = Camera.main.ScreenToWorldPoint(position);
+			GameObject go = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
+			go.transform.LookAt(position);    
+			Debug.Log(position);    
+			go.GetComponent<Rigidbody>().AddForce(go.transform.forward * 1000f);
+		}
+	
+		Destroy(GameObject.Find("Sphere(Clone)"), 5);
+	}
+
+}
